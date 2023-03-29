133c6354c892a74db9a3a122270d6f9d6dd2e032
erickyb <richardyb1985@gmail.com>
2023-03-28 20:03:09 -0500

    mejorando movies.controllers.js

--- src/movies/movies.controllers.js
+++ src/movies/movies.controllers.js
@@ -16,47 +16,23 @@ const movieBd = [
     "director": "David Fincher"
   },
 
-  // {
-  //   "id": 3,
-  //   "title": "La milla verde",
-  //   "description": "Esta película está basada en un relato de Stephen King y narra la historia de Paul Edgecomb, un hombre de avanzada edad que recuerda su experiencia como vigilante en la prisión de Cold Mountain en 1932.",
-  //   "year": 1999,
-  //   "director": "Frank Darabont"
-  // },
+  {
+    "id": 3,
+    "title": "La milla verde",
+    "description": "Esta película está basada en un relato de Stephen King y narra la historia de Paul Edgecomb, un hombre de avanzada edad que recuerda su experiencia como vigilante en la prisión de Cold Mountain en 1932.",
+    "year": 1999,
+    "director": "Frank Darabont"
+  },
 
-  // {
-  //   "id": 4,
-  //   "title": "Good Will Hunting",
-  //   "description": "Esta película narra la vida de Will, un joven rebelde con don privilegiado para las matemáticas. Cuando sus profesores descubren su talento, el protagonista tiene que enfrentarse a un gran dilema: seguir con su vida normal, con un trabajo común y sin preocupaciones, o escoger un nuevo camino y aprovechar su ingenio. MI PREFERIDA :)",
-  //   "year": 1997,
-  //   "director": "Gus Van Sant"
-  // }
+  {
+    "id": 4,
+    "title": "Good Will Hunting",
+    "description": "Esta película narra la vida de Will, un joven rebelde con don privilegiado para las matemáticas. Cuando sus profesores descubren su talento, el protagonista tiene que enfrentarse a un gran dilema: seguir con su vida normal, con un trabajo común y sin preocupaciones, o escoger un nuevo camino y aprovechar su ingenio. MI PREFERIDA :)",
+    "year": 1997,
+    "director": "Gus Van Sant"
+  }
 
 ]
-
-
-
-// let userId = 3
-
-// const findAllUsers = () => {
-//   return userDb
-// }
-
-// const findUserById = (id) => {
-//   const data = userDb.find(user => user.id === id)
-//   return data
-// }
-
-// const createUser = (userObj) => {
-//   const newUser = {
-//     id: userId++,
-//     name: userObj.name || 'Anonymous',
-//     email: userObj.email || 'example@example.com',
-//     password: userObj.password || 'root'
-//   }
-//   userDb.push(newUser)
-//   return newUser
-// }
 
 
 let movieId = 5
